aa44c836dde8d7ff51a1840889e31cecd3f7a354
Mustafa Kemal Çelik <106036880+mustafakclk@users.noreply.github.com>
2022-06-02 20:52:52 +0300

    Delete 2. hafta ödevler/js ödev directory

--- 2. hafta ödevler/js ödev/2.haftaödev3.js
+++
@@ -1,9 +0,0 @@
-var nameandSurname = {
-    "name": "MUSTAFA KEMAL "+"<br/>", 
-    "surname": "ÇELİK"+"<br/>",
-    "number": "699"
-}
-
-var jsonToString = JSON.stringify(nameandSurname)
-var parseJson = JSON.parse(jsonToString)
-document.write("Name: " + parseJson.name + "\t" + "Surname: " + parseJson.surname + "\t" + "Number: " + parseJson.number)
